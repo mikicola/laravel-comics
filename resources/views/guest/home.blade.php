5a57811c615cc7594cc386dae046dcadbfddf28f
mikicola <michela.colangelo@gmail.com>
2022-04-27 19:24:46 +0200

    struttura super base

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,19 @@
+{{-- home.blade richiama base.blade che richiama i partials  --}}
+
+@extends('guest.template.base')
+{{--
+1   title = cosa sotituisco
+2   dc - home = cosa inserisco --}}
+@section('title', 'DC - Home')
+
+
+{{--
+1   = cosa sotituisco
+2   = cosa inserisco (h1 e p) --}}
+@section('content')
+    <h1>prova</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi neque similique eveniet laborum omnis doloremque esse asperiores qui repudiandae fugiat illum, fugit ullam alias earum facere recusandae cum eos voluptatum.</p>
+
+    {{-- @dd($dc) --}}
+
+@endsection
---
+++ resources/views/guest/partials/footer.blade.php
@@ -0,0 +1,26 @@
+<footer>
+    <ul>
+        <h3>dc comics</h3>
+        <li><a href="#">a</a></li>
+        <li><a href="#">a</a></li>
+        <li><a href="#">a</a></li>
+    </ul>
+    <ul>
+        <h3>shop</h3>
+        <li><a href="#">a</a></li>
+        <li><a href="#">a</a></li>
+        <li><a href="#">a</a></li>
+    </ul>
+    <ul>
+        <h3>dc</h3>
+        <li><a href="#">a</a></li>
+        <li><a href="#">a</a></li>
+        <li><a href="#">a</a></li>
+    </ul>
+    <ul>
+        <h3>sites</h3>
+        <li><a href="#">a</a></li>
+        <li><a href="#">a</a></li>
+        <li><a href="#">a</a></li>
+    </ul>
+</footer>
